2d1a2a11d5a0115faf9b96af90bed00de1bafcf8
sandi <sandy.spyware@gmail.com>
2020-08-26 09:39:10 +0700

    first commit

---
+++ sandiFramework/sandiFramework/networkServices.swift
@@ -0,0 +1,17 @@
+//
+//  networkServices.swift
+//  sandiFramework
+//
+//  Created by SANDI on 26/08/20.
+//  Copyright © 2020 sample. All rights reserved.
+//
+
+import Foundation
+
+public class nerworkServices {
+    
+    public func fetchData() {
+        
+        print("this is sample rest api for fetch data")
+    }
+}
---
+++ sandiFramework/sandiFramework/sandiFramework.h
@@ -0,0 +1,19 @@
+//
+//  sandiFramework.h
+//  sandiFramework
+//
+//  Created by SANDI on 26/08/20.
+//  Copyright © 2020 sample. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sandiFramework.
+FOUNDATION_EXPORT double sandiFrameworkVersionNumber;
+
+//! Project version string for sandiFramework.
+FOUNDATION_EXPORT const unsigned char sandiFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sandiFramework/PublicHeader.h>
+
+
